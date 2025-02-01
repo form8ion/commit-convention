ab855fbeaba668df2a7e3a3582c6d0dbbb9a74d4
Matt Travi <programmer@travi.org>
2025-02-01 13:21:45 -0600

    test(unit): convert scaffolder tests to vitest
    for #458

---
+++ src/scaffolder.test.js
@@ -0,0 +1,96 @@
+import deepmerge from 'deepmerge';
+import {projectTypes} from '@form8ion/javascript-core';
+
+import any from '@travi/any';
+import {it, vi, describe, expect, beforeEach} from 'vitest';
+import {when} from 'jest-when';
+
+import {scaffold as scaffoldCommitlint} from './commitlint/index.js';
+import {scaffold as scaffoldCommitizen} from './commitizen/index.js';
+import {scaffold as scaffoldSemanticRelease} from './semantic-release/index.js';
+import scaffoldCommitConvention from './scaffolder.js';
+
+vi.mock('deepmerge');
+vi.mock('./semantic-release/scaffolder.js');
+vi.mock('./commitizen/scaffolder.js');
+vi.mock('./commitlint/index.js');
+
+describe('commit-convention scaffolder', () => {
+  const projectRoot = any.string();
+  const publishedProjectType = any.fromList([projectTypes.PACKAGE, projectTypes.CLI]);
+  const commitizenResults = any.simpleObject();
+  const semanticReleaseResults = any.simpleObject();
+  const mergedResults = any.simpleObject();
+  const conventionalCommitsBadge = {
+    img: 'https://img.shields.io/badge/Conventional%20Commits-1.0.0-yellow.svg',
+    text: 'Conventional Commits',
+    link: 'https://conventionalcommits.org'
+  };
+
+  beforeEach(() => {
+    when(scaffoldCommitizen).calledWith({projectRoot}).mockResolvedValue(commitizenResults);
+    when(scaffoldSemanticRelease).calledWith({projectRoot}).mockResolvedValue(semanticReleaseResults);
+  });
+
+  it('should configure the commit convention', async () => {
+    const commitlintConfig = any.simpleObject();
+    const commitlintResults = any.simpleObject();
+    when(scaffoldCommitlint).calledWith({projectRoot, config: commitlintConfig}).mockResolvedValue(commitlintResults);
+    when(deepmerge.all)
+      .calledWith([
+        commitizenResults,
+        commitlintResults,
+        {
+          vcsIgnore: {files: [], directories: []},
+          badges: {contribution: {'commit-convention': conventionalCommitsBadge}}
+        },
+        {}
+      ])
+      .mockReturnValue(mergedResults);
+
+    expect(await scaffoldCommitConvention({projectRoot, configs: {commitlint: commitlintConfig}}))
+      .toEqual(mergedResults);
+  });
+
+  it('should not configure commitlint if no config is provided', async () => {
+    when(deepmerge.all).calledWith([
+      commitizenResults,
+      {
+        vcsIgnore: {files: [], directories: []},
+        badges: {contribution: {'commit-convention': conventionalCommitsBadge}}
+      },
+      {}
+    ]).mockReturnValue(mergedResults);
+
+    expect(await scaffoldCommitConvention({projectRoot, configs: {}})).toEqual(mergedResults);
+    expect(scaffoldCommitlint).not.toHaveBeenCalled();
+  });
+
+  it('should configure semantic-release for publishable project types', async () => {
+    when(deepmerge.all).calledWith([
+      commitizenResults,
+      {
+        vcsIgnore: {files: [], directories: []},
+        badges: {contribution: {'commit-convention': conventionalCommitsBadge}}
+      },
+      semanticReleaseResults
+    ]).mockReturnValue(mergedResults);
+
+    expect(await scaffoldCommitConvention({projectRoot, projectType: publishedProjectType, configs: {}}))
+      .toEqual(mergedResults);
+  });
+
+  it('should not configure tools for the commit-convention for a sub-project', async () => {
+    expect(await scaffoldCommitConvention({pathWithinParent: any.string()})).toEqual({});
+  });
+
+  it('should only configure semantic-release for a sub-project when the project is publishable', async () => {
+    expect(
+      await scaffoldCommitConvention({
+        projectRoot,
+        pathWithinParent: any.string(),
+        projectType: publishedProjectType
+      })
+    ).toEqual(semanticReleaseResults);
+  });
+});
